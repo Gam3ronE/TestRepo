0293a1dd8a999dbf9ec9a5d039b6dbc49eae1a9e
Gam3ronE <graham@gamerone.net>
2014-01-26 21:28:00 +0000

    Added PHP Switch
    What is $a?

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>TestRepo</title>
+    <title>Gam3ronE</title>
     </head>
     <body>
     <h1>Gam3ronE</h1>
     <p>This is a test repo by Gam3ronE.</p>
+    <?php
+    
+    $a=5;
+    
+    switch($a) {
+        case 1:
+            echo '$a is 1.';
+            break;
+        case 2:
+        case 3:
+            echo '$a is somehwere between 2 and 3.';
+            break;
+        case 4:
+        case 5:
+        case 6:
+            echo '$a is somewhere between 4 and 6.';
+            break;
+    }
+        
     </body>
 </html>
